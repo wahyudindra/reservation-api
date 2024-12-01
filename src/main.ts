92849c3b27d957c85c45f16801a212dd42aea7ac
wahyudi.indra <wahyudiindra79@gmail.com>
2024-12-02 04:19:25 +0700

    chore: configure validation pipe and swaggerDocs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    app.enableCors();
+    app.useGlobalPipes(
+        new ValidationPipe({ whitelist: true, transform: true, transformOptions: { enableImplicitConversion: true } }),
+    );
+
+    const options = new DocumentBuilder()
+        .setTitle('Reservation API')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .setExternalDoc('Reservation API - Collection', '/docs-json')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
+    const host = process.env.HOST || 'localhost';
+    const port = process.env.PORT || 3000;
+    await app.listen(port as number, host);
+    console.info(`Server running on ${host}:${port}`);
 }
 bootstrap();
