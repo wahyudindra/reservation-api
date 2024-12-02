d615b1b789cee9e43e090714457d61964b2547f1
wahyudi.indra <wahyudiindra79@gmail.com>
2024-12-02 08:06:56 +0700

    fix(schema): set unique name on tables

--- prisma/seeders/tables.seeder.ts
+++ prisma/seeders/tables.seeder.ts
@@ -1,17 +1,21 @@
 import { Prisma } from './prisma.utils';
 import { Prisma as PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
-import { isNumberString } from 'class-validator';
+import * as dayjs from 'dayjs';
 
 export const run = async (loop: number) => {
     const data: PrismaClient.TableCreateManyInput[] = [];
-    const latestTable = await Prisma.instance.table.findFirst({ orderBy: { name: 'desc' } });
+    const latestTable = await Prisma.instance.table.findFirst({ orderBy: { createdAt: 'desc' } });
+    let now = dayjs();
 
     for (let i = 1; i <= loop; i++) {
+        now = now.add(1, 'second');
         data.push({
             isActive: faker.datatype.boolean(),
             name: String((Number(latestTable?.name) || 0) + i),
             qty: faker.helpers.rangeToNumber({ min: 2, max: 8 }),
+            createdAt: now.toDate(),
+            updatedAt: now.toDate(),
         });
     }
     await Prisma.instance.table.createMany({ data, skipDuplicates: true });
